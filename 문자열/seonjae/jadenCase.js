77835770cc12e6c8f137d504a2a69b1676ffc9f8
Sunjae95 <tjswo9502@gmail.com>
2021-09-09 01:17:35 +0900

    Fix: JadenCase 문제 리팩토링

--- 문자열/seonjae/jadenCase.js
+++ 문자열/seonjae/jadenCase.js
@@ -1,22 +1,42 @@
 // 내 풀이
-function solution(s) {
-    s = s.toLowerCase();
-    const splitedCase = s.split(" ");
-    const answer = splitedCase.reduce((pre, string) => {
-        if (string === "") return pre;
-        
-        const res = string[0].toUpperCase() + string.slice(1);
-        
-        return (pre += `${res} `);
-    }, "");
-    
-    return answer.trimEnd();
+function solution1(s) {
+  s = s.toLowerCase();
+  const splitedCase = s.split(" ");
+  const answer = splitedCase.reduce((pre, string) => {
+    if (string === "") return pre + " ";
+
+    const res = string[0].toUpperCase() + string.slice(1);
+
+    return (pre += `${res} `);
+  }, "");
+
+  return answer;
 }
 
 // 다른풀이
 function solution(s) {
   return s
     .split(" ")
-    .map((text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase())
+    .map(
+      (text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+    )
+    .join(" ");
+}
+
+//엣지 케이스 오류
+
+//고친 풀이
+const jadenCase = (string) => {
+  if (string === "") return "";
+
+  const res = string[0].toUpperCase() + string.slice(1);
+  return res;
+};
+
+function solution(s) {
+  return s
+    .toLowerCase()
+    .split(" ")
+    .map(jadenCase)
     .join(" ");
 }
