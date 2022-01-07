306f5cc3b94e02c4d3954979d8f421ee1e85a46b
SeungrokYoon <sonicrok93@gmail.com>
2022-01-07 17:44:25 +0900

    Fix: esline에 node.js 옵션 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,13 @@
-/*global module*/
 module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  plugins: ["prettier"],
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 13,
-    sourceType: "module",
   },
-  rules: { "prettier/prettier": "error", "no-unused-vars": "off" },
-};
+  rules: { 'prettier/prettier': 'error', 'no-unused-vars': 'off' },
+}
