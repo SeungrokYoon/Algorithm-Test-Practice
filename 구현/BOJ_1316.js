8e6fa7adff626823665971608075b848f0e33525
SeungrokYoon <sonicrok93@gmail.com>
2022-01-08 09:11:05 +0900

    refactor: 불필요한 순회 병합

--- 구현/BOJ_1316.js
+++ 구현/BOJ_1316.js
@@ -1,11 +1,5 @@
 const fs = require('fs')
 const [n, ...arr] = fs.readFileSync('/dev/stdin').toString().trim().split(/\s+/)
-// 직접 예시를 만들어 테스트 해 보고자 한다면, 디렉토리에 input.txt 테스트 입력 파일 생성 후, 아래 주석의 코드를 활용하면 됩니다!
-// const [n, ...arr] = fs
-//   .readFileSync(__dirname + '/input.txt', { encoding: 'utf8', flag: 'r' })
-//   .toString()
-//   .trim()
-//   .split(/\s+/)
 
 const solution = (n, arr) => {
   let answer = 0
@@ -19,18 +13,13 @@ const solution = (n, arr) => {
 const checkGroup = (target) => {
   const hashMap = {}
   let compressedTarget = target[0]
+  hashMap[compressedTarget] = 1
   for (let i = 1; i < target.length; i++) {
     const currentCh = target[i]
     if (compressedTarget[compressedTarget.length - 1] === currentCh) continue
+    if (Object.keys(hashMap).includes(currentCh)) return false
+    hashMap[currentCh] = 1
     compressedTarget += currentCh
-  }
-  for (let ch of compressedTarget) {
-    //Set 자료구조를 사용해도 될 듯?
-    if (Object.keys(hashMap).includes(ch)) {
-      return false
-    } else {
-      hashMap[ch] = 1
-    }
   }
   return true
 }
