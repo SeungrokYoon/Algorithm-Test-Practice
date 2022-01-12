07953d930160221d85bcce32a0f875082b47d37e
SeungrokYoon <sonicrok93@gmail.com>
2022-01-13 04:21:02 +0900

    Add:2022-01-13 괄호의 값 문제 해결

--- 재귀/BOJ_2504.js
+++ 재귀/BOJ_2504.js
@@ -1,26 +1,40 @@
 const fs = require('fs')
-// const arr = fs.readFileSync('/dev/stdin').toString().split(' ')
-const arr = fs
-  .readFileSync(__dirname + '/text.txt')
-  .toString()
-  .split(' ')
+const arr = fs.readFileSync('/dev/stdin').toString().trim().split('').reverse()
 
 const solution = (arr) => {
   let answer = 0
-  const findCorrectParenthesis = (arr) => {
-    const parenthesis = arr.join('')
-    if (arr[0] === ']' || arr[0] === ')') return 0
-    if (parenthesis === '[]') return 3
-    if (parenthesis === '()') return 2
-    //덩어리 찾기
-    let subArr = []
-    subArr.push(arr[0])
-    while (subArr.length) {
-      for (let i = 1; i < arr.length; i++) {
-        const popped = subArr.pop()
-        if (popped === '[' && arr[i] === ']') {
+  const recursion = (startParen) => {
+    let tempSum = 0
+    while (arr.length) {
+      const nextParen = arr.pop()
+      if (nextParen === '(' || nextParen === '[') {
+        const subRecursed = recursion(nextParen)
+        if (subRecursed) {
+          tempSum += subRecursed
+        } else {
+          //subRecuresed가 0이면 이후 모든 상위 작업이 종료되어야 하기 때문에 0 리턴해주기
+          return 0
         }
+      } else if (startParen + nextParen === '()') {
+        return 2 * Math.max(tempSum, 1)
+      } else if (startParen + nextParen === '[]') {
+        return 3 * Math.max(tempSum, 1)
+      } else {
+        //종류가 다른 괄호로 닫힐 때
+        return 0
       }
     }
   }
+  while (arr.length) {
+    const subResult = recursion(arr.pop())
+    if (subResult) {
+      answer += subResult
+    } else {
+      answer = 0
+      break
+    }
+  }
+  return answer
 }
+
+console.log(solution(arr))
