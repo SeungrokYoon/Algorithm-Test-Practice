696c3837962abbb40cac5c4e825c4327bae050fd
SeungrokYoon <sonicrok93@gmail.com>
2022-02-02 13:51:41 +0900

    Add: 2022-02-02 점프 문제해결

--- 동적계획법/BOJ_1890.js
+++ 동적계획법/BOJ_1890.js
@@ -5,31 +5,28 @@ const input = require('fs')
   .split('\n')
 
 const N = input.shift() * 1
-const map = input.map((i) => i.split(' ').map((j) => +j))
+const table = input.map((i) => i.split(' ').map((j) => +j))
 const temp = new Array(N).fill(0)
 const dp = temp.fill(0).map((i) => new Array(N).fill(0))
+dp[0][0] = 1
 
 const solution = () => {
-  const stack = []
-  if (map[0][0] < N) {
-    stack.push([0, map[0][0]])
-    stack.push([map[0][0], 0])
-    dp[0][map[0][0]] += 1
-    dp[map[0][0]][0] += 1
-  }
-  while (stack.length > 0) {
-    const [x, y] = stack.pop()
-    const nextHop = map[x][y]
-    if (x + nextHop < N && nextHop !== 0) {
-      stack.push([x + nextHop, y])
-      dp[x + nextHop][y] += dp[x][y]
-    }
-    if (y + nextHop < N && nextHop !== 0) {
-      stack.push([x, y + nextHop])
-      dp[x][y + nextHop] += dp[x][y]
+  for (let row = 0; row < N; row++) {
+    for (let col = 0; col < N; col++) {
+      const nextHop = table[row][col]
+      if (dp[row][col] === 0 || nextHop === 0 || (row === N - 1 && col === N - 1)) continue
+      if (row + nextHop < N) {
+        const newInt = BigInt(dp[row + nextHop][col]) + BigInt(dp[row][col])
+        dp[row + nextHop][col] = BigInt(newInt)
+      }
+      if (col + nextHop < N) {
+        const newInt = BigInt(dp[row][col + nextHop]) + BigInt(dp[row][col])
+        dp[row][col + nextHop] = BigInt(newInt)
+      }
     }
   }
-  console.log(dp[N - 1][N - 1])
+  const result = dp[N - 1][N - 1]
+  console.log(result.toString())
 }
 
 solution()
