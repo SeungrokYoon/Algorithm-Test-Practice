8d2a01fd7ff273f7669d1c66064ad0714cb6fb59
SeungrokYoon <sonicrok93@gmail.com>
2022-02-13 15:30:28 +0900

    Add: 2022-02-13 조이스틱 2차 풀이 및 문제 해결 (제출 성공)

--- 그리디/PG_조이스틱.js
+++ 그리디/PG_조이스틱.js
@@ -1,52 +1,58 @@
-//https://programmers.co.kr/learn/courses/30/lessons/42860
 function solution(name) {
-  let answer = 0
-  const result = []
-  let start = new Array(name.length).fill('A')
+  const answer = 0
+  //상하 움직임 계산
+  const charCodeArr = name.split('').map((ch) => {
+    const charCode = ch.charCodeAt(0) - 65
+    return Math.min(charCode, 26 - charCode)
+  })
+  const upDown = charCodeArr.reduce((a, b) => a + b, 0)
+  //모두A라면 그냥 끝내면 됨
+  if (charCodeArr.reduce((a, b) => a + b, 0) === 0) {
+    return 0
+  }
 
-  const chCodeArr = name
-    .split('')
-    .map((ch) => ch.charCodeAt(0) - 65)
-    .map((chCode) => Math.min(chCode, 26 - chCode))
+  //순방향 (우측): 0번 인덱스에서 가장 먼 A가아닌 철자의 인덱스
+  let toRight = upDown
+  for (let i = name.length - 1; i >= 0; i--) {
+    if (charCodeArr[i] !== 0) {
+      toRight += i
+      break
+    }
+  }
 
-  const findClosestNum = (currentIndex) => {
-    //currentIndex로부터 A가 아닌 가장 가까운 문자를 지닌 인덱스 찾기-< A는 지나쳐도 되니까.
-    let distance = 1
-    let leftIndex = currentIndex
-    let rightIndex = currentIndex
-    let closestNextIndex = name.length + 1
-    while (distance < name.length) {
-      leftIndex = (currentIndex - distance + name.length) % name.length
-      rightIndex = (currentIndex + distance) % name.length
-      if (name[rightIndex] !== start[rightIndex] && name[leftIndex] !== start[leftIndex]) {
-        closestNextIndex = rightIndex
-        return { closestNextIndex: closestNextIndex, distance: distance }
-      } else if (name[rightIndex] === start[rightIndex] && name[leftIndex] !== start[leftIndex]) {
-        closestNextIndex = Math.min(leftIndex, closestNextIndex)
-        return { closestNextIndex: closestNextIndex, distance: distance }
-      } else if (name[rightIndex] !== start[rightIndex] && name[leftIndex] === start[leftIndex]) {
-        closestNextIndex = Math.min(rightIndex, closestNextIndex)
-        return { closestNextIndex: closestNextIndex, distance: distance }
-      } else {
-        distance++
+  //역방향 (좌측): 0번 인덱스에서 우측으로 가장 가까운 A가 아닌 철자의 인덱스까지 역방향 거리
+  let toLeft = upDown
+  for (let i = 0; i < name.length; i++) {
+    if (charCodeArr[i] !== 0) {
+      toLeft += name.length - i
+      break
+    }
+  }
+
+  let toRightLeft = name.length - 1
+  let toLeftRight = name.length - 1
+  //순->역 : 0번 인덱스부터 시작하여 차례대로 가장 작은 순->역 거리를 구한다.
+  //역->순 : 0번 인덱스부터 시작하여 차례대로 가장 작은 순->역 거리를 구한다.
+  //순->역의 거리 : 인덱스까지의 거리 + 해당 인덱스에서 우측으로 가장 가까운 A가 아닌 철자의 인덱스까지 역방향 거리 = 2 * 인덱스 + 역방향 거리
+  //역->순의 거리 : 인덱스까지의 거리 + 최 우측부터 (해당 인덱스에서 우측으로 가장 가까운 A가 아닌 철자의 인덱스) 까지의 거리 * 2 = 인덱스 + 역방향 거리 * 2
+  for (let i = 0; i < name.length; i++) {
+    //가장 가까운 오른쪽 찾기
+    let tempToRightLeft = 0
+    let tempToLeftRight = 0
+    for (let j = i + 1; j < name.length; j++) {
+      if (charCodeArr[j] !== 0) {
+        tempToRightLeft += i + name.length - j
+        tempToLeftRight += 2 * (name.length - j)
+        break
       }
     }
-    return { closestNextIndex: name.length, distance: name.length }
+    tempToRightLeft += i
+    tempToLeftRight += i
+    toRightLeft = Math.min(toRightLeft, tempToRightLeft)
+    toLeftRight = Math.min(toLeftRight, tempToLeftRight)
   }
-  for (let nextIndex = 0; nextIndex < name.length; nextIndex++) {
-    while (start.join('') !== name) {
-      answer += chCodeArr[nextIndex]
-      start[nextIndex] = name[nextIndex]
-      const next = findClosestNum(nextIndex)
-      if (next.closestNextIndex === name.length) return answer
-      nextIndex = next.closestNextIndex
-      answer += next.distance
-    }
-    result.push(answer)
-    answer = 0
-  }
-  return result
-}
+  toRightLeft += upDown
+  toLeftRight += upDown
 
-console.log(solution('BAJAAAAAAZ'))
-console.log(solution('BBBBAAAAABA'))
+  return Math.min(toRight, toLeft, toRightLeft, toLeftRight)
+}
