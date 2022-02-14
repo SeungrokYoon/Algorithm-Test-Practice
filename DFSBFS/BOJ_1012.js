4f14e75f097901277d0fd9a00d75a62bc40ad745
SeungrokYoon <sonicrok93@gmail.com>
2022-02-15 05:06:08 +0900

    Add: 2022-02-15 유기농 배추 2차 문제해결(DFS)

--- DFSBFS/BOJ_1012.js
+++ DFSBFS/BOJ_1012.js
@@ -9,15 +9,12 @@ const T = input.shift() * 1
 for (let test = 0; test < T; test++) {
   const [M, N, K] = input.shift().split(' ').map(Number)
   //그래프 작성
-  const graph = new Array(N)
-  for (let i = 0; i < N; i++) {
-    graph[i] = new Array(M).fill(0)
-  }
+  const graph = Array.from({ length: N }, () => Array.from({ length: M }, () => 0))
   for (let i = 0; i < K; i++) {
     const [col, row] = input.shift().split(' ').map(Number)
     graph[row][col] = 1
   }
-  //각 좌표별 BFS
+  //각 좌표별 DFS
   let count = 0
   const dRow = [1, -1, 0, 0]
   const dCol = [0, 0, 1, -1]
@@ -25,9 +22,9 @@ for (let test = 0; test < T; test++) {
     for (let col = 0; col < M; col++) {
       if (graph[row][col] !== 1) continue
       count++
-      const queue = [[row, col]]
-      while (queue.length) {
-        const [row, col] = queue.shift()
+      const stack = [[row, col]]
+      while (stack.length) {
+        const [row, col] = stack.pop()
         for (let i = 0; i < 4; i++) {
           const newRow = dRow[i] + row
           const newCol = dCol[i] + col
@@ -39,7 +36,7 @@ for (let test = 0; test < T; test++) {
             graph[newRow][newCol] === 1
           ) {
             graph[newRow][newCol] = 2 //방문표시
-            queue.push([newRow, newCol])
+            stack.push([newRow, newCol])
           }
         }
       }
