f7c962ea8fd9d9afc0d32969bf7044e70bfab3f4
SeungrokYoon <sonicrok93@gmail.com>
2022-02-14 15:29:57 +0900

    2022-02-14 숨바꼭질 문제해결

--- DFSBFS/BOJ_1697.js
+++ DFSBFS/BOJ_1697.js
@@ -1,60 +1,3 @@
-class Node {
-  constructor(position, time) {
-    this.position = position
-    this.time = time
-    this.previous = null
-    this.next = null
-  }
-}
-class Deque {
-  constructor() {
-    this.head = null
-    this.tail = null
-    this.size = 0
-  }
-  popFromLeft() {
-    if (this.head !== null) {
-      this.size -= 1
-      const popped = this.head
-      if (this.head === this.tail) {
-        //원소가 하나일 떄
-        this.head = null
-        this.tail = null
-        return popped
-      }
-      this.head = this.head.next
-      return popped
-    }
-  }
-  popFromRight() {
-    if (this.head !== null) {
-      this.size -= 1
-      const popped = this.tail
-      if (this.head === this.tail) {
-        //원소가 하나일 떄
-        this.head = null
-        this.tail = null
-        return popped
-      }
-      this.tail = this.tail.previous
-      this.tail.next = null
-      return popped
-    }
-  }
-  enqueue(position, time) {
-    this.size += 1
-    const node = new Node(position, time)
-    if (this.head === null) {
-      this.head = node
-      this.tail = node
-    } else {
-      this.tail.next = node
-      node.previous = this.tail
-      this.tail = node
-    }
-  }
-}
-
 const [N, K] = require('fs')
   .readFileSync(__dirname + '/test.txt')
   .toString()
@@ -62,16 +5,34 @@ const [N, K] = require('fs')
   .split(' ')
   .map(Number)
 
+const MAX = 100001
 const solution = (n, k) => {
-  const deque = new Deque()
-  deque.enqueue(n, 0)
-  while (deque.size >= 0) {
-    const poppedFromLeft = deque.popFromLeft()
-    if (poppedFromLeft.position === k) return poppedFromLeft.time
-    deque.enqueue(poppedFromLeft.position - 1, poppedFromLeft.time + 1)
-    deque.enqueue(poppedFromLeft.position + 1, poppedFromLeft.time + 1)
-    deque.enqueue(poppedFromLeft.position * 2, poppedFromLeft.time + 1)
+  const table = new Array(MAX).fill(-1)
+  const queue = []
+  table[n] = 0
+  queue.push(n)
+  let index = 0
+  while (index < queue.length) {
+    const popped = queue[index]
+    const time = table[popped]
+    if (popped === k) {
+      break
+    }
+    if (table[popped - 1] === -1 && popped - 1 >= 0) {
+      table[popped - 1] = time + 1
+      queue.push(popped - 1)
+    }
+    if (table[popped + 1] === -1 && popped + 1 < MAX) {
+      table[popped + 1] = time + 1
+      queue.push(popped + 1)
+    }
+    if (table[popped * 2] === -1 && popped * 2 < MAX) {
+      table[popped * 2] = time + 1
+      queue.push(popped * 2)
+    }
+    index++
   }
+  return table[k]
 }
 
 console.log(solution(N, K))
