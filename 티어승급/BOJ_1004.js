92e7f3bd176e8673babdd1016a4e374ec336ec66
SeungrokYoon <sonicrok93@gmail.com>
2022-02-16 04:22:10 +0900

    Add: 2022-02-16 어린왕자 2차 문제풀이 (문제해결)

--- 티어승급/BOJ_1004.js
+++ 티어승급/BOJ_1004.js
@@ -10,9 +10,7 @@ for (let i = 0; i < T; i++) {
   const [x1, y1, x2, y2] = input.shift().split(' ').map(Number)
   const n = input.shift() * 1
   const planets = []
-  let nIncludingStart = 0
-  let nIncludingEnd = 0
-  let nIncludingBoth = 0
+  let count = 0
 
   for (let planet = 0; planet < n; planet++) {
     planets.push(input.shift().split(' ').map(Number))
@@ -20,20 +18,17 @@ for (let i = 0; i < T; i++) {
 
   //출발점을 포함하는 행성계 개수 + 도착점을 포함하는 행성계 개수 - 2*공통 행성계 개수
   const isInPlanet = (px, py, cx, cy, r) => {
-    return cx - r < px && px < cx + r && cy - r < py && py < cy + r
+    //해당 점이 원의 바깥에 있는지 없는지는 원의 중심과 점 사이의 거리를 구하면된다
+    return (px - cx) * (px - cx) + (py - cy) * (py - cy) < r * r
   }
 
   for (const planet of planets) {
     const [cx, cy, r] = planet
     const includesStart = isInPlanet(x1, y1, cx, cy, r)
     const includesEnd = isInPlanet(x2, y2, cx, cy, r)
-    if (includesStart && includesEnd) {
-      nIncludingBoth++
-    } else if (includesStart && !includesEnd) {
-      nIncludingStart++
-    } else if (!includesStart && includesEnd) {
-      nIncludingEnd++
-    }
+    if (includesStart) count++
+    if (includesEnd) count++
+    if (includesStart && includesEnd) count -= 2
   }
-  console.log(nIncludingStart + nIncludingEnd - 2 * nIncludingBoth)
+  console.log(count)
 }
