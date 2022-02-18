ce43b059b7aefa271c19ff5a186e3e567c59f257
SeungrokYoon <sonicrok93@gmail.com>
2022-02-18 12:47:53 +0900

    ADd:2022-02-18 N과 M 2차 문제해결(순열 알고리즘)

--- 티어승급/BOJ_15649.js
+++ 티어승급/BOJ_15649.js
@@ -5,45 +5,26 @@ const [N, M] = require('fs')
   .split(' ')
   .map(Number)
 
-//조합 순열 진짜 쥐약이네...
+//이 문제는 순열 nPr을 물어보는 문제
 const solution = () => {
   const numArr = Array.from({ length: N }, (_, index) => index + 1)
-  const combinations = (elements) => {
-    if (elements.length === 0) return [[]]
-    const firstEl = elements[0]
-    const rest = elements.slice(1)
-    const combsWithoutFirst = combinations(rest)
-    const combsWithFirst = []
-    combsWithoutFirst.forEach((comb) => combsWithFirst.push([firstEl, ...comb]))
-    return [...combsWithoutFirst, ...combsWithFirst]
-  }
+  function permutation(arr, selectNum) {
+    let result = []
+    if (selectNum === 1) return arr.map((v) => [v])
 
-  const permutations = (elements) => {
-    if (elements.length === 0) return [[]]
-    const firstEl = elements[0]
-    const rest = elements.slice(1)
-    const permsWithoutFirst = permutations(rest)
-    const allPermutations = []
-    permsWithoutFirst.forEach((perm) => {
-      for (let i = 0; i <= perm.length; i++) {
-        const permWithFirst = [...perm.slice(0, i), firstEl, ...perm.slice(i)]
-        allPermutations.push(permWithFirst)
-      }
+    arr.forEach((v, idx, arr) => {
+      const fixer = v
+      const restArr = arr.filter((_, index) => index !== idx)
+      const permuationArr = permutation(restArr, selectNum - 1)
+      const combineFixer = permuationArr.map((v) => [fixer, ...v])
+      result.push(...combineFixer)
     })
-    return allPermutations
+    return result
   }
-  const result = []
-  const combis = combinations(numArr).filter((comb) => comb.length === M)
-  combis.forEach((comb) => {
-    const permu = permutations(comb)
-    result.push(...permu)
+  const result = permutation(numArr, M)
+  result.forEach((permu) => {
+    console.log(permu.join(' '))
   })
-  result
-    .map((permu) => permu.join(' '))
-    .sort()
-    .forEach((permu) => {
-      console.log(permu)
-    })
 }
 
 solution()
