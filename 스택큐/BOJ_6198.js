f3cd9bd511dc8b8746555645076a712f6e1196b4
SeungrokYoon <sonicrok93@gmail.com>
2022-02-22 14:29:29 +0900

    Add:2022-02-22 옥상 정원 꾸미기 문제 해결

--- 스택큐/BOJ_6198.js
+++ 스택큐/BOJ_6198.js
@@ -8,25 +8,12 @@ const [N, ...arr] = require('fs')
 const solution = (arr, N) => {
   let answer = 0
   const stack = []
-  const last = arr.pop()
-  stack.push(last)
-  while (arr.length) {
-    const topOfArr = arr[arr.length - 1]
-    let temp = 0
-    while (stack.length) {
-      const poppedFromStack = stack.pop()
-      if (topOfArr <= poppedFromStack) {
-        stack.push(poppedFromStack)
-        break
-      } else {
-        arr.push(poppedFromStack)
-        temp += 1
-      }
+  for (let i = 0; i < N; i++) {
+    while (stack.length && stack[stack.length - 1] <= arr[i]) {
+      stack.pop()
     }
-    for (let i = 0; i < temp + 1; i++) {
-      stack.push(arr.pop())
-    }
-    answer += temp
+    stack.push(arr[i])
+    answer += stack.length - 1
   }
   return answer
 }
