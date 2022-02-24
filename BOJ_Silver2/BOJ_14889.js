a8e67f0bbb358a9f6ae00ce0a174eb44ba32c014
SeungrokYoon <sonicrok93@gmail.com>
2022-02-24 19:03:09 +0900

    Add: 2022-02-24 스타트와 링크 2차 문제해결

--- BOJ_Silver2/BOJ_14889.js
+++ BOJ_Silver2/BOJ_14889.js
@@ -7,46 +7,34 @@ const input = require('fs')
 const N = input.shift() * 1
 const M = N / 2
 const graph = input.map((str) => str.split(' ').map(Number))
-const members = Array.from({ length: N }, (_, i) => i + 1)
+// const members = Array.from({ length: N }, (_, i) => i + 1)
+const members = Array.from({ length: N }, () => false)
+let min = 100 * N * N
 
-function combination(arr, selectNum) {
-  const result = []
-  if (selectNum === 1) return arr.map((v) => [v])
-  arr.forEach((v, idx, arr) => {
-    const fixed = v
-    const restArr = arr.slice(idx + 1)
-    const combinationArr = combination(restArr, selectNum - 1)
-    const combineFix = combinationArr.map((v) => [fixed, ...v])
-    result.push(...combineFix)
-  })
-  return result
-}
-
-const answer = []
-const combis = combination(members, M)
-for (const startTeam of combis) {
-  const linkTeam = []
-  //링크팀 만들기
-  for (const member of members) {
-    if (!startTeam.includes(member)) {
-      linkTeam.push(member)
+const makeTeam = (index, count) => {
+  const start = []
+  const link = []
+  let startScore = 0
+  let linkScore = 0
+  if (count === M) {
+    //팀이 완성됨
+    members.forEach((v, i) => (v ? start.push(i) : link.push(i)))
+    for (let i = 0; i < M; i++) {
+      for (let j = 0; j < M; j++) {
+        startScore += graph[start[i]][start[j]]
+        linkScore += graph[link[i]][link[j]]
+      }
     }
+    min = Math.min(Math.abs(startScore - linkScore), min)
+    return
   }
-  //startCombi 만들기
-  const startCombi = combination(startTeam, 2)
-  let startSum = 0
-  for (const combi of startCombi) {
-    const [i, j] = combi
-    startSum += graph[i - 1][j - 1] + graph[j - 1][i - 1]
+  for (let i = index; i < N; i++) {
+    if (members[i]) continue
+    members[i] = true
+    makeTeam(i, count + 1)
+    members[i] = false
   }
-
-  //linkCombi 만들기
-  const linkCombi = combination(linkTeam, 2)
-  let linkSum = 0
-  for (const combi of linkCombi) {
-    const [i, j] = combi
-    linkSum += graph[i - 1][j - 1] + graph[j - 1][i - 1]
-  }
-  answer.push(Math.abs(startSum - linkSum))
 }
-console.log(Math.min(...answer))
+
+makeTeam(0, 0)
+console.log(min)
