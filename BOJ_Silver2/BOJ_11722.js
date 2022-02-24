ba126efd0f6225773ed2a8815486b98349214df2
SeungrokYoon <sonicrok93@gmail.com>
2022-02-24 11:39:18 +0900

    Refactor: 최대값 로직 수정

--- BOJ_Silver2/BOJ_11722.js
+++ BOJ_Silver2/BOJ_11722.js
@@ -8,13 +8,15 @@ const N = input.shift() * 1
 const arr = input.shift().split(' ').map(Number)
 
 const solution = (N, arr) => {
+  let max = 1
   const dp = Array.from({ length: N }, () => 1)
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < i; j++) {
       if (arr[j] <= arr[i]) continue
       dp[i] = Math.max(dp[i], dp[j] + 1)
+      max = Math.max(dp[i], max)
     }
   }
-  return Math.max(...dp)
+  return max
 }
 console.log(solution(N, arr))
