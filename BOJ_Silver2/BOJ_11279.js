04bde9f216d42a098cacc71dbf6843b406d17ca2
SeungrokYoon <sonicrok93@gmail.com>
2022-02-26 22:38:28 +0900

    Add: 2022-02-26 최대 힙 1차 구현 실패

---
+++ BOJ_Silver2/BOJ_11279.js
@@ -0,0 +1,74 @@
+class MaxHeap {
+  constructor() {
+    this.heap = []
+  }
+  insert(v) {
+    this.heap.push(v)
+    this.upHeap(this.heap.length - 1)
+  }
+  upHeap(pos) {
+    let parentIndex = parseInt((pos - 1) / 2)
+    while (this.heap[pos] > this.heap[parentIndex]) {
+      const temp = this.heap[pos]
+      this.heap[pos] = this.heap[parentIndex]
+      this.heap[parentIndex] = temp
+      pos = parentIndex
+      parentIndex = parseInt((pos - 1) / 2)
+    }
+  }
+  getMax() {
+    if (this.heap.length === 0) return 0
+    if (this.heap.length === 1) {
+      const max = this.heap[0]
+      this.heap.pop()
+      return max
+    }
+    const max = this.heap[0]
+    this.heap[0] = this.heap[this.heap.length - 1]
+    this.heap.pop()
+    this.downHeap(0)
+    return max
+  }
+  downHeap(pos) {
+    while (pos < Math.floor(this.heap.length / 2)) {
+      let childIndex = pos * 2 + 1 //왼쪽자식의 인덱스 pos 가 Math.floor(this.heap.length/2) 라 무조건 왼쪽 자식은 있음
+      //자식 중 더 큰 자식 구하기,
+      if (childIndex + 1 < this.heap.length && this.heap[childIndex] < this.heap[childIndex + 1])
+        childIndex++ //오른쪽 자식이 존재하고, 오른쪽 자식이 더 크면 오른쪽을 선택
+      if (this.heap[pos] >= this.heap.length[childIndex]) break
+      //만약 선택된 노드값이 교환할 수 없다면 break
+      console.log('   pos changed with children ', pos, childIndex)
+      console.log(`   heap`, this.heap)
+      const temp = this.heap[pos]
+      this.heap[pos] = this.heap[childIndex]
+      this.heap[childIndex] = temp
+      pos = childIndex
+    }
+  }
+  print() {
+    console.log(this.heap)
+  }
+  size() {
+    console.log(this.heap.length)
+  }
+}
+
+const input = require('fs')
+  .readFileSync(__dirname + '/test.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(Number)
+
+const N = input.shift()
+const heap = new MaxHeap()
+let result = ''
+for (let i = 0; i < N; i++) {
+  if (input[i] === 0) {
+    result += `${heap.getMax()}\n`
+  } else {
+    heap.insert(input[i])
+  }
+  heap.print()
+}
+console.log(result.trim())
