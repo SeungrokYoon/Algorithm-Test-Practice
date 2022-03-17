e50fe15cc3b62ee8ea1d729fb532d6d6cb9ba058
SeungrokYoon <sonicrok93@gmail.com>
2022-03-17 15:50:00 +0900

    Add: 2022-03-17 서강 그라운드 문제 해결

--- 최단경로/BOJ_14938.js
+++ 최단경로/BOJ_14938.js
@@ -1,35 +1,27 @@
 const input = require('fs').readFileSync('test.txt').toString().trim().split('\n')
-const [n, m, r] = input[0].split(' ').map(Number)
+const [N, M, R] = input[0].split(' ').map(Number)
 const itemsPerVertex = [0].concat(input[1].split(' ').map(Number))
-const edges = input.slice(2, m + 2).map((s) => s.split(' ').map(Number))
+const edges = input.slice(2, R + 2).map((s) => s.split(' ').map(Number))
 
-const floydWarshall = (n, edges) => {
+const floydWarshall = (N, edges) => {
   //create graph
-  const adjMatrix = new Map()
-
-  for (let i = 1; i < n + 1; i++) {
-    adjMatrix.set(i, new Map())
-    for (let j = 1; j < n + 1; j++) {
-      adjMatrix.get(i).set(j, Infinity)
-    }
-  }
+  const adjMatrix = Array.from({ length: N + 1 }, (_, i) =>
+    Array.from({ length: N + 1 }, (_, j) => (i === j ? 0 : Infinity)),
+  )
 
   for (const [v1, v2, weight] of edges) {
-    const newValue = Math.min(adjMatrix.get(v1).get(v2), weight)
-    adjMatrix.get(v1).set(v2, newValue)
-    adjMatrix.get(v2).set(v1, newValue)
-  }
-
-  for (let i = 1; i < n + 1; i++) {
-    adjMatrix.get(i).set(i, 0)
+    const smaller = Math.min(adjMatrix[v1][v2], weight)
+    adjMatrix[v1][v2] = smaller
+    adjMatrix[v2][v1] = smaller
   }
 
   //O(V^3) traverse
-  for (let m = 1; m < n + 1; m++) {
-    for (let s = 1; s < n + 1; s++) {
-      for (let e = 1; e < n + 1; e++) {
-        if (adjMatrix.get(s).get(e) > adjMatrix.get(s).get(m) + adjMatrix.get(m).get(e)) {
-          adjMatrix.get(s).set(e, adjMatrix.get(s).get(m) + adjMatrix.get(m).get(e))
+  for (let m = 1; m < N + 1; m++) {
+    for (let s = 1; s < N + 1; s++) {
+      for (let e = 1; e < N + 1; e++) {
+        if (adjMatrix[s][e] > adjMatrix[s][m] + adjMatrix[m][e]) {
+          adjMatrix[s][e] = adjMatrix[s][m] + adjMatrix[m][e]
+          adjMatrix[e][s] = adjMatrix[s][m] + adjMatrix[m][e]
         }
       }
     }
@@ -38,13 +30,13 @@ const floydWarshall = (n, edges) => {
 }
 
 const solution = () => {
-  const adjMatrix = floydWarshall(n, edges)
+  const adjMatrix = floydWarshall(N, edges)
   let answer = 0
-  for (let i = 1; i < n + 1; i++) {
+  for (let i = 1; i < N + 1; i++) {
     let tempSum = 0
-    for (const [j, dist] of adjMatrix.get(i).entries()) {
-      tempSum += dist <= r ? itemsPerVertex[j] : 0
-    }
+    adjMatrix[i].forEach((dist, vertex) => {
+      tempSum += dist <= M ? itemsPerVertex[vertex] : 0
+    })
     answer = Math.max(answer, tempSum)
   }
   return answer
