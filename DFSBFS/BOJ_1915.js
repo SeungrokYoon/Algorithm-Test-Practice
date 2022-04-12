15cdcc08306685c188ef54d90dbef281681e0a8b
Seungrok <sonicrok93@gmail.com>
2022-04-12 20:12:02 +0900

    Add: 2022-04-12 가장 큰 정사각형 문제해결(dp)

--- DFSBFS/BOJ_1915.js
+++ DFSBFS/BOJ_1915.js
@@ -6,61 +6,29 @@ const input = require('fs')
 
 const [N, M] = input.shift().split(' ').map(Number)
 const graph = input.map((s) => s.split('').map(Number))
-let answer = 1
-const copyGraph = (graph) => {
-  const copied = []
-  graph.forEach((row) => copied.push([...row]))
-  return copied
-}
-const findSquare = (row, col, graph) => {
-  let tempAnswer = 1
-  //오른쪽, 아래, 아래대각선으로만 탐색
-  let pointer = 0
-  const dRow = [0, 1, 1]
-  const dCol = [1, 0, 1]
-  const copied = graph
-  const queue = []
-  queue.push(row)
-  queue.push(col)
-  while (pointer < queue.length) {
-    const currentRow = queue[pointer++]
-    const currentCol = queue[pointer++]
-    for (let i = 0; i < 3; i++) {
-      const nextRow = currentRow + dRow[i]
-      const nextCol = currentCol + dCol[i]
-      if (
-        0 > nextRow ||
-        N <= nextRow ||
-        0 > nextCol ||
-        M <= nextCol ||
-        copied[nextRow][nextCol] === 0
-      )
-        break
-      if (
-        0 > nextRow ||
-        N <= nextRow ||
-        0 > nextCol ||
-        M <= nextCol ||
-        copied[nextRow][nextCol] === 2
-      )
-        continue
-      if (nextRow - row === nextCol - col) tempAnswer++
-      copied[nextRow][nextCol] = 2
-      queue.push(nextRow)
-      queue.push(nextCol)
-    }
-  }
-  answer = Math.max(tempAnswer, answer)
+let answer = 0
+
+const generateDpTable = (N, M) => {
+  return Array.from({ length: N + 1 }, () => Array.from({ length: M + 1 }, () => 0))
 }
 
-const solution = () => {
+const renewSquare = (row, col, graph, dp) => {
+  const dpRow = row + 1
+  const dpCol = col + 1
+  const newEdge = Math.min(dp[dpRow - 1][dpCol - 1], dp[dpRow - 1][dpCol], dp[dpRow][dpCol - 1]) + 1
+  dp[dpRow][dpCol] = newEdge
+  answer = Math.max(answer, newEdge)
+}
+
+const solution = (N, M, graph) => {
+  const dp = generateDpTable(N, M)
   for (let row = 0; row < N; row++) {
     for (let col = 0; col < M; col++) {
       if (graph[row][col] !== 1) continue
-      findSquare(row, col, graph)
+      renewSquare(row, col, graph, dp)
     }
   }
 }
 
-solution()
+solution(N, M, graph)
 console.log(answer * answer)
