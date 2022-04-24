95bf4577ac30e56917be036526ca1606630e4faf
Seungrok <sonicrok93@gmail.com>
2022-04-24 21:46:56 +0900

    refactor: 링크드 리스트로 표편집 정합성 통과

--- 구현/PG_표편집.js
+++ 구현/PG_표편집.js
@@ -16,62 +16,91 @@ class LinkedList {
   }
   generateLinkedNodes(n, k) {
     for (let i = 0; i < n; i++) {
-      const node = new Node(n)
-      if (this.tail === null) {
+      const node = new Node(i)
+      if (k === i) {
+        this.currentNode = node
+      }
+      if (this.head === null) {
         //첫 번째 노드
         this.head = node
+        this.tail = node
+        continue
       }
-      if (k === i) this.currentNode = node
       this.tail.next = node
       node.previous = this.tail
       this.tail = node
     }
   }
-  recoverDeletedNode(node) {
+  recoverDeletedNode() {
     const recoveredNode = this.deletedNodes.pop()
+    //prevNode, nextNode도 리스트에서 삭제된 친구들일 수도 있다.링크드 리스트는 그래도 괜춘
+    //0,1,(2),(3),(4),5,6,7 이고, 4,3,2순서로 삭제되었을 때를 생각해보면
+    //[4{prev:3,next:5},3{prev:2,next:5},2{prev:1,next:5}]
     const prevNode = recoveredNode.previous
     const nextNode = recoveredNode.next
-    prevNode.next = recoveredNode
-    if (nextNode !== null) nextNode.prevNode = recoveredNode
+    //head, tail, currentNode에 대한 갱신해야함
+    if (prevNode) prevNode.next = recoveredNode
+    if (nextNode) nextNode.previous = recoveredNode
+    if (prevNode === this.tail) {
+      this.tail = recoveredNode
+    }
+    if (nextNode === this.head) {
+      this.head = recoveredNode
+    }
   }
-  deleteCurrentNode(value) {
-    const previousNode = this.currentNode.previous
-    const nextNode = this.currentNode.next
-    previousNode.next = nextNode
-    nextNode.previous = previousNode
+  deleteCurrentNode() {
     this.deletedNodes.push(this.currentNode)
-    this.currentNode = nextNode !== null ? nextNode : previousNode
-  }
-  moveDown(num) {
-    for (let i = 0; i < num; i++) {
-      this.currentNode = this.currentNode.previous
+    if (this.currentNode === this.head) {
+      this.head = this.currentNode.next
+      this.head.previous = null
+      this.currentNode = this.head
+    } else if (this.currentNode === this.tail) {
+      this.tail = this.currentNode.previous
+      this.tail.next = null
+      this.currentNode = this.tail
+    } else {
+      const previousNode = this.currentNode.previous
+      const nextNode = this.currentNode.next
+      previousNode.next = nextNode
+      nextNode.previous = previousNode
+      this.currentNode = nextNode
     }
   }
   moveUp(num) {
     for (let i = 0; i < num; i++) {
+      this.currentNode = this.currentNode.previous
+    }
+  }
+  moveDown(num) {
+    for (let i = 0; i < num; i++) {
       this.currentNode = this.currentNode.next
     }
   }
-}
-
-const binarySearch = (arr, key) => {
-  let start = 0
-  let end = arr.length - 1
-  const mid = Math.floor((start + end) / 2)
-  if (arr[mid] === key) {
-    return mid
-  } else if (arr[mid] > key) {
-    end = mid - 1
-  } else {
-    start = mid + 1
+  printList() {
+    let result = ''
+    let cNode = this.head
+    while (cNode !== null) {
+      result += cNode.value + ' '
+      cNode = cNode.next
+    }
+    console.log(result)
   }
-  return -1
+  display(n) {
+    const table = new Array(n).fill('X')
+    let result = ''
+    let cNode = this.head
+    while (cNode !== null) {
+      table[cNode.value] = 'O'
+      cNode = cNode.next
+    }
+    result = table.join('')
+    return result
+  }
 }
 
 function solution(n, k, cmd) {
   var answer = ''
   const linkedList = new LinkedList(n, k)
-  console.log(linkedList)
   for (const command of cmd) {
     const commandType = command[0]
     if (commandType === 'U') {
@@ -83,11 +112,10 @@ function solution(n, k, cmd) {
     } else if (commandType === 'C') {
       linkedList.deleteCurrentNode()
     } else {
-      //Z
       linkedList.recoverDeletedNode()
     }
+    linkedList.display()
   } //for
+  answer = linkedList.display(n)
   return answer
 } //solution
-
-console.log(solution(8, 2, ['D 2', 'C', 'U 3', 'C', 'D 4', 'C', 'U 2', 'Z', 'Z']))
