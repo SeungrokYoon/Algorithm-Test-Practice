e60547fc1d72e3bce79ff89f8be8869f2d5706ab
Seungrok <sonicrok93@gmail.com>
2022-04-28 19:10:21 +0900

    refactor: 후보키 문제 유일성 체크 로직 함수화

--- 완전탐색/PG_후보키.js
+++ 완전탐색/PG_후보키.js
@@ -12,6 +12,22 @@ const combinationDFS = (visited, arr, index, depth, search, result, pool) => {
   }
 }
 
+const checkIsUnique = (columnList, relation) => {
+  const searched = []
+  relation.forEach((aRelation) => {
+    let compressed = ''
+    for (const column of columnList) {
+      compressed += aRelation[column]
+    }
+    searched.push(compressed)
+  })
+  //유일성통과
+  const searchedSet = new Set(searched)
+  //하나 빼고 했는데도 유일성이 유지된다면 false
+  if (searched.length === searchedSet.size) return true
+  return false
+}
+
 function solution(relation) {
   //해당 컬럼을 기준으로 유일한 값들을 지니고 있는지 파악하면 해당 컬름은 유일하다.
   var answer = 0
@@ -25,51 +41,26 @@ function solution(relation) {
     combinationDFS(visited, columnArr, 0, 0, search, allColumnCombination, pool)
   }
   allColumnCombination.forEach((aCombi) => {
-    const searched = []
-    relation.forEach((aRelation) => {
-      let compressed = ''
-      for (const column of aCombi) {
-        compressed += aRelation[column]
-      }
-      searched.push(compressed)
-    })
-    //유일성통과
-    const searchedSet = new Set(searched)
-    if (searched.length === searchedSet.size) candidateKeys.push(aCombi)
+    const isUnique = checkIsUnique(aCombi, relation)
+    if (isUnique) candidateKeys.push(aCombi)
   })
   //유일성 통과한 것들 중 최소성을 찾는다.겹치지 않는 녀석들
-  console.log(candidateKeys)
-  const trueKeySet = new Set()
   candidateKeys.forEach((key) => {
     //최소성 통과해보기
     //길이가 1이면 최소임 그러니까 노상관
     if (key.length === 1) {
       answer++
-      trueKeySet.add(`${key}`)
-      console.log(key)
       return
     }
     //하나씩 빼보기
-    let remainUnique = true
-    console.log('key', key)
+    let isMinimum = true
     for (let i = 0; i < key.length; i++) {
       const tempKey = [...key.slice(0, i), ...key.slice(i + 1)]
-      console.log('temp', tempKey)
-      const searched = []
-      relation.forEach((aRelation) => {
-        let compressed = ''
-        for (const column of tempKey) {
-          compressed += aRelation[column]
-        }
-        searched.push(compressed)
-      })
-      //유일성통과
-      const searchedSet = new Set(searched)
+      const isUnique = checkIsUnique(tempKey, relation)
       //하나 빼고 했는데도 유일성이 유지된다면 false
-      if (searched.length === searchedSet.size) remainUnique = false
+      if (isUnique) isMinimum = false
     }
-    if (remainUnique) {
-      console.log(key, 'isUnique')
+    if (isMinimum) {
       answer++
     }
   })
