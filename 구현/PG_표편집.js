13842ff2521f681604c3d94191862406283be64d
Seungrok <sonicrok93@gmail.com>
2022-05-03 16:31:16 +0900

    refactor: switch문으로 수정

--- 구현/PG_표편집.js
+++ 구현/PG_표편집.js
@@ -4,52 +4,65 @@ function solution(n, k, cmd) {
   const linkedList = {}
   const LEFTEND = -1
   const RIGHTEND = n
+  let move = 0,
+    prev = 0,
+    next = 0,
+    now = 0,
+    deletedRow = null
   for (let i = 0; i < n; i++) {
     linkedList[i] = { prev: i - 1, next: i + 1 }
   }
   const deleted = []
   for (const command of cmd) {
     const commandType = command[0]
-    if (commandType === 'U') {
-      const move = Number(command.split(' ')[1])
-      for (let i = 0; i < move; i++) {
-        k = linkedList[k].prev
-      }
-    } else if (commandType === 'D') {
-      const move = Number(command.split(' ')[1])
-      for (let i = 0; i < move; i++) {
-        k = linkedList[k].next
-      }
-    } else if (commandType === 'C') {
-      const { prev, next } = linkedList[k]
-      deleted.push({ prev, next, now: k })
-      checkList[k] = 'X'
-      if (next === RIGHTEND) {
-        k = linkedList[k].prev
-      } else {
-        k = linkedList[k].next
-      }
-      if (prev === LEFTEND) {
-        //선택된 행이 맨 위의 행임
-        linkedList[next].prev = prev
-      } else if (next === RIGHTEND) {
-        //선택된 행이 맨 아래 행임
-        linkedList[prev].next = next
-      } else {
-        linkedList[prev].next = next
-        linkedList[next].prev = prev
-      }
-    } else {
-      const { prev, next, now } = deleted.pop()
-      checkList[now] = 'O'
-      if (prev === LEFTEND) {
-        linkedList[next].prev = now
-      } else if (next === RIGHTEND) {
-        linkedList[prev].next = now
-      } else {
-        linkedList[prev].next = now
-        linkedList[next].prev = now
-      }
+    switch (commandType) {
+      case 'U':
+        move = Number(command.split(' ')[1])
+        for (let i = 0; i < move; i++) {
+          k = linkedList[k].prev
+        }
+        break
+      case 'D':
+        move = Number(command.split(' ')[1])
+        for (let i = 0; i < move; i++) {
+          k = linkedList[k].next
+        }
+        break
+      case 'C':
+        prev = linkedList[k].prev
+        next = linkedList[k].next
+        deleted.push({ prev, next, now: k })
+        checkList[k] = 'X'
+        if (next === RIGHTEND) {
+          k = linkedList[k].prev
+        } else {
+          k = linkedList[k].next
+        }
+        if (prev === LEFTEND) {
+          //선택된 행이 맨 위의 행임
+          linkedList[next].prev = prev
+        } else if (next === RIGHTEND) {
+          //선택된 행이 맨 아래 행임
+          linkedList[prev].next = next
+        } else {
+          linkedList[prev].next = next
+          linkedList[next].prev = prev
+        }
+        break
+      default:
+        deletedRow = deleted.pop()
+        prev = deletedRow.prev
+        next = deletedRow.next
+        now = deletedRow.now
+        checkList[now] = 'O'
+        if (prev === LEFTEND) {
+          linkedList[next].prev = now
+        } else if (next === RIGHTEND) {
+          linkedList[prev].next = now
+        } else {
+          linkedList[prev].next = now
+          linkedList[next].prev = now
+        }
     }
   } //for
   answer = checkList.join('')
