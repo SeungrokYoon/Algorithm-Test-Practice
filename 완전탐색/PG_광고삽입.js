262032926e8b619128a93c7747348143004587ca
Seungrok <sonicrok93@gmail.com>
2022-05-16 14:59:01 +0900

    refactor: 변수명 수정을 통한 가독성 개선

--- 완전탐색/PG_광고삽입.js
+++ 완전탐색/PG_광고삽입.js
@@ -16,34 +16,33 @@ function solution(play_time, adv_time, logs) {
   var answer = ''
   const playTime = toSeconds(play_time)
   const advTime = toSeconds(adv_time)
-  //imosArr에 누적합 변화율을 O(1)시간으로 저장을 해놓고, 누적합을 저장하는 dp를 통해 누적합 구할 때 O(1)만에 구할 수 있도록 합니다.
-  const imosArr = new Array(playTime + 1).fill(0)
-  const dp = new Array(playTime + 1).fill(0)
+  //imos에 누적합 변화율을 O(1)시간으로 저장을 해놓고, O(n)시간에 누적합을 구한다.
+  //imos의 누적합을 저장하는 dp를 통해 누적합 구할 때 구간의 누적합을 O(1)만에 구할 수 있도록 합니다.
+  const imosDP = new Array(playTime + 1).fill(0)
+  const sumDP = new Array(playTime + 1).fill(0)
   //imos 채우기
   logs.forEach((log) => {
     const [startTime, endTime] = log.split('-').map(toSeconds)
-    imosArr[startTime]++
-    imosArr[endTime]--
+    imosDP[startTime]++
+    imosDP[endTime]--
   })
-  //imosArr, dp 채우기
-  dp[0] = imosArr[0]
+  //imosDP, sumDP 채우기
+  sumDP[0] = imosDP[0]
   for (let i = 1; i < playTime + 1; i++) {
-    imosArr[i] = imosArr[i] + imosArr[i - 1]
-    dp[i] = dp[i - 1] + imosArr[i]
+    imosDP[i] = imosDP[i - 1] + imosDP[i]
+    sumDP[i] = sumDP[i - 1] + imosDP[i]
   }
-  //dp에서 순회하며 찾아보기 최대 누적 시청자 수인 구간을 찾기
+  //구간의 누적합을 찾아보면서 최대 누적 시청자수 지점의 최초 지점을 기록
   let theEarlestStartTime = 0
-  let maxAcc = dp[advTime]
-  for (let i = advTime - 1; i < imosArr.length; i++) {
-    const advEnd = i
-    const advStart = i - advTime + 1 //이래야 advStart+1 부터 advEnd까지 합해서 i 간격이 됨
-    const acc = dp[advEnd] - dp[advStart - 1]
-    if (acc > maxAcc) {
-      theEarlestStartTime = advStart
-      maxAcc = acc
+  let maxSum = sumDP[advTime]
+  for (let adEnd = advTime - 1; adEnd < imosDP.length; adEnd++) {
+    const adStart = adEnd - advTime + 1
+    const subSum = sumDP[adEnd] - sumDP[adStart - 1]
+    if (subSum > maxSum) {
+      theEarlestStartTime = adStart
+      maxSum = subSum
     }
   }
-  console.log(theEarlestStartTime)
   answer = toHHMMSS(theEarlestStartTime)
   return answer
 }
