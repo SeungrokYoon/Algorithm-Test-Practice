e3babea77d4d2f03feb9c1291e0242c55252f37b
Seungrok <sonicrok93@gmail.com>
2022-05-26 09:33:04 +0900

    refactor: 불필요한 로직 제거

--- 최단경로/BOJ_11657.js
+++ 최단경로/BOJ_11657.js
@@ -5,79 +5,12 @@ const input = require('fs')
   .split('\n')
   .map((s) => s.split(' ').map(Number))
 
-class MinHeap {
-  constructor() {
-    this.heap = []
-  }
-  push(newValue) {
-    this.heap.push(newValue)
-    this.upheap()
-  }
-  upheap() {
-    let childIndex = this.heap.length - 1
-    let parentIndex = Math.floor((childIndex - 1) / 2)
-    while (parentIndex >= 0 && this.heap[parentIndex].weight > this.heap[childIndex].weight) {
-      const temp = this.heap[parentIndex]
-      this.heap[parentIndex] = this.heap[childIndex]
-      this.heap[childIndex] = temp
-      childIndex = parentIndex
-      parentIndex = Math.floor((childIndex - 1) / 2)
-    }
-  }
-  pop() {
-    if (!this.heap.length) return null
-    if (this.heap.length === 1) return this.heap.pop()
-    const root = this.heap[0]
-    this.heap[0] = this.heap[this.heap.length - 1]
-    this.heap.pop()
-    this.downheap()
-    return root
-  }
-  downheap() {
-    if (this.heap.length === 0) return null
-    let parentIndex = 0
-    while (2 * parentIndex + 1 < this.heap.length) {
-      let childIndex = 2 * parentIndex + 1
-      if (
-        childIndex + 1 < this.heap.length &&
-        this.heap[childIndex].weight > this.heap[childIndex + 1].weight
-      )
-        childIndex++
-      if (this.heap[parentIndex].weight <= this.heap[childIndex].weight) break
-      const temp = this.heap[parentIndex]
-      this.heap[parentIndex] = this.heap[childIndex]
-      this.heap[childIndex] = temp
-      parentIndex = childIndex
-    }
-  }
-  size() {
-    return this.heap.length
-  }
-  display() {
-    console.log(this.heap)
-  }
-}
-
-const genGraph = (vertex, edges) => {
-  const graph = new Map()
-  for (let i = 1; i < vertex + 1; i++) {
-    graph.set(i, new Map())
-  }
-  edges.forEach((edge) => {
-    const [u, v, w] = edge
-    graph.get(u).set(v, w)
-  })
-  return graph
-}
-
 const [N, M] = input[0]
 const edges = input.slice(1)
 const distance = Array.from({ length: N + 1 }, () => Infinity)
 
 const belmanFord = (start, distance) => {
-  const pq = new MinHeap()
   distance[start] = 0
-  pq.push({ current: start, weight: 0 })
   //벨만 포드에서는 정점개수-1 만큼 edge relaxation 진행 후, 마지막 한 번을 더 해줌.
   for (let v = 1; v < N + 1; v++) {
     //모든 간선을 살펴보기
