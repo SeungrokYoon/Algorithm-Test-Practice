3ea73973cb62fc21f587df73dd23a169a3741875
Seungrok <sonicrok93@gmail.com>
2023-03-26 00:18:59 +0900

    feat: Solve BOJ_5622

---
+++ Bronze/BOJ_5622.js
@@ -0,0 +1,32 @@
+const arr = require('fs').readFileSync('dev/stdin').toString().trim().split('')
+const dict = {
+  A: 2,
+  B: 2,
+  C: 2,
+  D: 3,
+  E: 3,
+  F: 3,
+  G: 4,
+  H: 4,
+  I: 4,
+  J: 5,
+  K: 5,
+  L: 5,
+  M: 6,
+  N: 6,
+  O: 6,
+  P: 7,
+  Q: 7,
+  R: 7,
+  S: 7,
+  T: 8,
+  U: 8,
+  V: 8,
+  W: 9,
+  X: 9,
+  Y: 9,
+  Z: 9,
+}
+
+const answer = arr.reduce((prev, curr) => prev + dict[curr], 0) + arr.length
+console.log(answer)
