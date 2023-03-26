04ec4143b35bb4b8a5c28f3fe3cbf92a974541af
Seungrok <sonicrok93@gmail.com>
2023-03-26 23:20:26 +0900

    refactor: Change  BOJ_10812 to use JS built-in methods

--- Bronze/BOJ_10812.js
+++ Bronze/BOJ_10812.js
@@ -1,36 +1,20 @@
 const [[M, N], ...infoArr] = require('fs')
-  .readFileSync('test/test.txt')
+  .readFileSync(0)
   .toString()
   .trim()
   .split('\n')
   .map((s) => s.split(' ').map(Number))
 
 const arr = new Array(M + 1).fill().map((_, index) => index)
-console.log('arr', arr)
 
 const answer = infoArr.reduce((prevArr, currInput) => {
   const [i, j, k] = currInput
-  const newArr = []
-  let tempArr = []
-  prevArr.forEach((num, index) => {
-    if (index < i) {
-      newArr.push(num)
-    } else if (index >= i && index < k) {
-      tempArr.push(num)
-    } else if (index >= k && index < j) {
-      newArr.push(num)
-    } else {
-      if (index === j + 1) {
-        tempArr.forEach((el) => newArr.push(el))
-        tempArr = []
-      }
-      newArr.push(num)
-      if (index === M) {
-        tempArr.forEach((el) => newArr.push(el))
-      }
-    }
-  })
-  return newArr
+  const head = prevArr.slice(0, i)
+  const frontMiddle = prevArr.slice(i, k)
+  const rearMiddle = prevArr.slice(k, j + 1)
+  const tail = prevArr.slice(j + 1)
+
+  return [...head, ...rearMiddle, ...frontMiddle, ...tail]
 }, arr)
 
 console.log(answer.slice(1).join(' '))
