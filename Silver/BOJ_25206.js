a9ac036dafb94ba8e6e000baa7b7eb208e3892ed
Seungrok <sonicrok93@gmail.com>
2023-03-30 15:57:34 +0900

    error: reference Error while node /Users/seungrokyoon/Desktop/coding_study_repo/Silver/BOJ_25206.js

---
+++ Silver/BOJ_25206.js
@@ -0,0 +1,29 @@
+const input = require('fs').readFileSync(0).toString().trim().split('\n')
+
+const info = { totalGrade: 0, totalTime: 0 }
+
+const convertGrade2Score = (grade) => {
+  const gradeMap = {
+    'A+': 4.5,
+    A0: 4.0,
+    'B+': 3.5,
+    B0: 3.0,
+    'C+': 2.5,
+    C0: 2.0,
+    'D+': 1.5,
+    D0: 1.0,
+    F: 0.0,
+  }
+  return gradeMap[grade] || 0
+}
+
+input.reduce((acc, line) => {
+  if (grade === 'P' || grade === 'F') return acc
+  const [subject, time, grade] = line.split(' ')
+  acc.totalGrade += convertGrade2Score(grade)
+  acc.totalTime += time * 1
+  return acc
+}, info)
+
+const answer = info.totalGrade / info.totalTime
+console.log(answer)
