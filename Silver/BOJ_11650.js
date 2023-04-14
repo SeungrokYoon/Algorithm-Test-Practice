8aa150f1de6d56c5a9233f55aac183d5e68f09e7
Seungrok <sonicrok93@gmail.com>
2023-04-14 18:26:28 +0900

    feat: Solve BOJ_11650 sort 함수의 compare 함수 응용

---
+++ Silver/BOJ_11650.js
@@ -0,0 +1,11 @@
+const [n, ...arr] = require('fs').readFileSync(0).toString().trim().split('\n')
+arr.sort((prev, curr) => {
+  const [x1, y1] = prev.split(' ').map(Number)
+  const [x2, y2] = curr.split(' ').map(Number)
+  return x1 - x2 || y1 - y2
+})
+const answer = arr.reduce((acc, curr) => {
+  return acc + curr + '\n'
+}, '')
+
+console.log(answer)
