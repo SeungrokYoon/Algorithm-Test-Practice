d0875f713dde4cddbc01a4b5983aca75c4a7b104
Seungrok <sonicrok93@gmail.com>
2023-05-30 16:10:54 +0900

    feat: BOJ_18258 큐2 3차 풀이- 시간초과16%
    아마 pop 케이스에서의 shift 연산때문에 발생하는 것으로 추측이 된다

--- Silver/BOJ_18258.js
+++ Silver/BOJ_18258.js
@@ -5,24 +5,24 @@ const input = require('fs')
   .split('\n')
 
 input.shift()
-
+const answer = []
 input.reduce((acc, curr) => {
   const value = curr.split(' ')[1] * 1
   switch (curr) {
     case 'front':
-      console.log(acc.length ? acc[0] : -1)
+      answer.push(acc.length ? acc[0] : -1)
       break
     case 'back':
-      console.log(acc.length ? acc[acc.length - 1] : -1)
+      answer.push(acc.length ? acc[acc.length - 1] : -1)
       break
     case 'size':
-      console.log(acc.length)
+      answer.push(acc.length)
       break
     case 'empty':
-      console.log(Number(acc.length === 0))
+      answer.push(Number(acc.length === 0))
       break
     case 'pop':
-      console.log(acc.length ? acc[0] : -1)
+      answer.push(acc.length ? acc[0] : -1)
       acc.shift()
       break
     default:
@@ -30,3 +30,5 @@ input.reduce((acc, curr) => {
   }
   return acc
 }, [])
+
+console.log(answer.join('\n'))
