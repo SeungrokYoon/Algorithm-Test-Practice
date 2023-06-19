9a6e35fd31c55b3e704e8ae07512f4e121a41ff0
Seungrok <sonicrok93@gmail.com>
2023-06-19 16:47:19 +0900

    feat: Solve BOJ_14888

--- Silver/BOJ_14888_연산자끼워넣기.js
+++ Silver/BOJ_14888_연산자끼워넣기.js
@@ -1,60 +1,47 @@
-const MAX = Number.MAX_SAFE_INTEGER
-const MIN = Number.MIN_SAFE_INTEGER
-
 const input = require('fs')
   .readFileSync(process.platform === 'linux' ? 'dev/stdin' : 'test/test.txt')
   .toString()
   .trim()
   .split('\n')
-const N = input[0]
+
+const MAX = Number.MAX_SAFE_INTEGER
+const MIN = Number.MIN_SAFE_INTEGER
+const N = +input[0]
 const numArr = input[1].split(' ').map(Number)
-const [plus, minus, multiply, divide] = input[2].split(' ').map(Number)
-const operators = (
-  '+'.repeat(plus) +
-  '-'.repeat(minus) +
-  '*'.repeat(multiply) +
-  '/'.repeat(divide)
-).split('')
-const operatorPool = []
-const result = { max: MIN, min: MAX }
 
-const dfs = (currDepth, targetDepth) => {
+let [add, sub, mul, div] = input[2].split(' ').map(Number)
+let max = MIN
+let min = MAX
+
+const dfs = (currDepth, targetDepth, calcResult) => {
   if (currDepth === targetDepth) {
-    const calcResult = numArr.reduce((acc, curr, i) => {
-      if (i === 0) {
-        return acc + curr
-      } else {
-        if (operatorPool[i - 1] === '/') {
-          return acc > 0 ? Math.floor(acc / curr) : -1 * Math.floor((-1 * acc) / curr)
-        } else {
-          if (operatorPool[i - 1] === '+') {
-            return acc + curr
-          }
-          if (operatorPool[i - 1] === '-') {
-            return acc - curr
-          }
-          if (operatorPool[i - 1] === '*') {
-            return acc * curr
-          }
-        }
-      }
-    }, 0)
-
-    result.max = result.max ? Math.max(result.max, calcResult) : calcResult
-    result.min = result.min ? Math.min(result.min, calcResult) : calcResult
+    max = Math.max(max, calcResult)
+    min = Math.min(min, calcResult)
     return
   }
-  for (let i = 0; i < operators.length; i++) {
-    const operator = operators[i]
-    if (operator === 'v') continue
-    operatorPool.push(operators[i])
-    operators[i] = 'v'
-    dfs(currDepth + 1, targetDepth)
-    operatorPool.pop()
-    operators[i] = operator
+
+  if (add > 0) {
+    add -= 1
+    dfs(currDepth + 1, targetDepth, calcResult + numArr[currDepth])
+    add += 1
+  }
+  if (sub > 0) {
+    sub -= 1
+    dfs(currDepth + 1, targetDepth, calcResult - numArr[currDepth])
+    sub += 1
+  }
+  if (mul > 0) {
+    mul -= 1
+    dfs(currDepth + 1, targetDepth, calcResult * numArr[currDepth])
+    mul += 1
+  }
+  if (div > 0) {
+    div -= 1
+    dfs(currDepth + 1, targetDepth, parseInt(calcResult / numArr[currDepth]))
+    div += 1
   }
 }
 
-dfs(0, operators.length)
-console.log(result.max)
-console.log(result.min)
+dfs(1, N, numArr[0])
+console.log(max === 0 ? 0 : max)
+console.log(min === 0 ? 0 : min)
