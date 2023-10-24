8c9c1e0c898fea3d07503d811ed79a4a5c2098c1
SeungrokYoon <sonicrok93@gmail.com>
2023-10-24 14:59:51 +0900

    feat: BOJ_11569 신호등 메모리초과

--- Platinum/BOJ_11569.js
+++ Platinum/BOJ_11569.js
@@ -4,12 +4,13 @@ const input = require('fs')
   .trim()
   .split('\n')
 
-input.shift()
+let remainingTests = +input.shift()
 
 class Node {
-  constructor(idx, val) {
+  constructor(idx, val, prev) {
     this.idx = idx
     this.val = val
+    this.prev = prev
   }
 }
 
@@ -17,8 +18,8 @@ class MinHeap {
   constructor() {
     this.heap = []
   }
-  insert(idx, num) {
-    const nextNode = new Node(idx, num)
+  insert(idx, num, prev) {
+    const nextNode = new Node(idx, num, prev)
     this.heap.push(nextNode)
     this.upHeap()
   }
@@ -81,50 +82,111 @@ class MinHeap {
   }
 }
 
-function solution(start, end, matrix, intervals) {
-  const dpTable = Array.from({ length: matrix.length }, () => Infinity)
-  const minHeap = new MinHeap()
-  const visited = Array.from({ length: matrix.length }, () => false)
-  visited[0] = true
-  minHeap.insert(start, 0)
+class Graph {
+  constructor(N) {
+    this.heapq = new MinHeap()
+    this.adjMatrix = Array.from({ length: N + 1 }, (_, i) => [])
+    this.dpTable = Array.from({ length: N + 1 }, () => Infinity)
+    this.visited = Array.from({ length: N + 1 }, () => false)
+    this.interval = [0]
+  }
+  init(edges, intervals) {
+    edges.forEach((el) => {
+      const [v, t, distance] = el
+      this.adjMatrix[t].push({ v, distance })
+      this.adjMatrix[v].push({ v, distance })
+    })
+    this.interval = [...this.interval, ...intervals]
+  }
+}
 
-  while (visited.some((el) => el === false)) {
-    const poppedNode = minHeap.pop()
-    if (visited[poppedNode.idx]) continue
-    for (let i = 1; i < matrix.length; i++) {
-      if (visited[i]) continue
-      visited[poppedNode.idx] = true
-      const fromPoppedToI = matrix[poppedNode.idx][i]
-      if (dpTable[i] > poppedNode.val + fromPoppedToI) {
-        const nextDistance = poppedNode.val + fromPoppedToI
-        dpTable[i] = nextDistance
-        minHeap.insert(i, nextDistance)
+function solution(start, end, matrix, intervals) {
+  const minHeap = new MinHeap()
+  const dpTable = Array.from({ length: matrix.length }, () => Infinity)
+  const visited = Array.from({ length: matrix.length }, () => false)
+  dpTable[start] = 0
+  visited[0] = true
+  visited[start] = true
+  for (let i = 1; i < matrix.length; i++) {
+    if (matrix[start][i] === Infinity) continue
+    minHeap.insert(i, matrix[start][i], start)
+    dpTable[i] = matrix[start][i]
+  }
+
+  while (!minHeap.isEmpty()) {
+    const routingNode = minHeap.pop()
+    if (visited[routingNode.idx] === true) continue
+    visited[routingNode.idx] = true
+    const connectedNodes = matrix[routingNode.idx]
+      .map((el, i) => [i, el])
+      .filter((el) => el[1] !== Infinity && el[1] !== 0)
+    const routingOrderOfPrevNode = connectedNodes.findIndex((el) => el[0] === routingNode.prev)
+    for (let targetNodeNumber = 1; targetNodeNumber < matrix.length; targetNodeNumber++) {
+      const unvisitable =
+        visited[targetNodeNumber] === true || matrix[routingNode.idx][targetNodeNumber] === Infinity
+      if (unvisitable) continue
+      const interValOfRouting = intervals[targetNodeNumber - 1]
+      const timeForPrevNodeToPassRoutingNode = getValidTime(
+        routingNode.val,
+        routingOrderOfPrevNode,
+        interValOfRouting,
+        connectedNodes.length,
+      )
+      const nextDist = timeForPrevNodeToPassRoutingNode + matrix[routingNode.idx][targetNodeNumber]
+      const updatable = dpTable[targetNodeNumber] > nextDist
+      if (updatable) {
+        dpTable[targetNodeNumber] = nextDist
+        minHeap.insert(targetNodeNumber, nextDist, routingNode.idx)
       }
     }
   }
-  return dpTable[end]
+  return dpTable[end] === Infinity ? -1 : dpTable[end]
+}
+
+const getValidTime = (
+  currTime,
+  routingOrderOfPrevNode,
+  intervalOfRoutingNode,
+  nOfConnectedNodes,
+) => {
+  let startTime = routingOrderOfPrevNode * intervalOfRoutingNode
+  let found = false
+  let validTime = 0
+  const cycleTime = intervalOfRoutingNode * nOfConnectedNodes
+
+  while (!found) {
+    if (startTime <= currTime && currTime < startTime + intervalOfRoutingNode) {
+      found = true
+      validTime = currTime
+    } else if (startTime - cycleTime + intervalOfRoutingNode <= currTime && currTime < startTime) {
+      validTime = startTime
+      found = true
+    } else {
+      startTime += cycleTime
+    }
+  }
+  return validTime
 }
 
 const getAnswer = () => {
-  const answer = []
+  let answer = ''
   const data = input.map((l) => l.split(' ').map(Number))
-
   let currI = 0
-  while (currI < data.length) {
+  while (remainingTests--) {
     const [N, M, S, D] = data[currI]
-    const intersections = data.slice(currI, currI + M + 1)
-    const intervals = data[currI + M + 1]
     const adjMatrix = Array.from({ length: N + 1 }, (_, i) =>
       Array.from({ length: N + 1 }, (_, j) => (i === j ? 0 : Infinity)),
     )
-    intersections.forEach(([v, t, distance]) => {
+    for (let i = currI + 1; i < currI + M + 1; i++) {
+      const [v, t, distance] = data[i]
       adjMatrix[t][v] = distance
       adjMatrix[v][t] = distance
-    })
-    answer.push(solution(S, D, adjMatrix, intervals))
+    }
+    const intervals = data[currI + M + 1]
+    answer += solution(S, D, adjMatrix, intervals) + '\n'
     currI += M + 2
   }
-  return answer
+  return answer.trim()
 }
 
-console.log(getAnswer().join('\n'))
+console.log(getAnswer())
