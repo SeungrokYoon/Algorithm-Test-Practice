be06addbf5b1ae1128562c3035208636de48d6d7
SeungrokYoon <sonicrok93@gmail.com>
2023-11-09 01:55:32 +0900

    feat: Memory exceeds in BOJ_2346

--- Silver/BOJ_2346_풍선터뜨리기.js
+++ Silver/BOJ_2346_풍선터뜨리기.js
@@ -5,88 +5,26 @@ const input = require('fs')
   .split('\n')
 
 const N = +input[0]
-const data = input[1].split(' ').map(Number)
+const arr = input[1]
+  .split(' ')
+  .map(Number)
+  .map((v, i) => ({ index: i, move: v, shot: false }))
 
-class Node {
-  constructor(value, index) {
-    this.value = value
-    this.index = index
-    this.prev = null
-    this.next = null
-    this.shot = false
-  }
-  getPrev() {
-    return this.prev
-  }
-  getNext() {
-    return this.next
-  }
-  shoot() {
-    this.shot = true
+let answer = ''
+let currI = 0
+let remaining = N
+
+while (remaining) {
+  remaining--
+  arr[currI].shot = true
+  answer += currI + 1 + ' '
+  let { move } = arr[currI]
+  let counter = Math.abs(arr[currI].move)
+  while (counter && remaining) {
+    const nextI = move > 0 ? (currI + 1) % N : (currI + N - 1) % N
+    if (!arr[nextI].shot) counter--
+    currI = nextI
   }
 }
 
-class Deque {
-  constructor() {
-    this.head = null
-    this.rear = null
-    this.length = 0
-  }
-  pushRear(num, i) {
-    const nextNode = new Node(num, i)
-    if (this.length === 0) {
-      this.head = nextNode
-      this.rear = nextNode
-    } else {
-      this.rear.next = nextNode
-      nextNode.prev = this.rear
-      this.rear = nextNode
-    }
-    this.length++
-  }
-}
-
-class BalloonTable {
-  constructor(arr, N) {
-    this.remaining = N
-    this.deque = new Deque()
-    this.curr = null
-    this.history = []
-    this.init(arr)
-  }
-  init(data) {
-    data.forEach((v, i) => {
-      this.deque.pushRear(v, i)
-    })
-    this.deque.head.prev = this.deque.rear
-    this.deque.rear.next = this.deque.head
-    this.curr = this.deque.head
-  }
-  moveToNextUnshotBalloon() {
-    this.curr.shoot()
-    this.history.push(this.curr.index + 1)
-    this.remaining--
-    let move = Math.abs(this.curr.value)
-    const direction = this.curr.value > 0 ? 1 : -1
-    while (move > 0 && this.remaining) {
-      if (direction > 0) {
-        if (!this.curr.getNext().shot) {
-          move--
-        }
-        this.curr = this.curr.getNext()
-      } else if (direction < 0) {
-        if (!this.curr.getPrev().shot) {
-          move--
-        }
-        this.curr = this.curr.getPrev()
-      }
-    }
-  }
-}
-
-const bt = new BalloonTable(data, N)
-for (let i = 0; i < N; i++) {
-  bt.moveToNextUnshotBalloon()
-}
-
-console.log(bt.history.join(' '))
+console.log(answer.trim())
