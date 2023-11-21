30bad80e80b737dcae63cd78fe512352ff584396
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2023-11-22 00:03:42 +0900

    [Bronze V] Title: 사파리월드, Time: 160 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.js
@@ -0,0 +1,8 @@
+const [N, M] = require('fs')
+  .readFileSync(process.platform === 'linux' ? 'dev/stdin' : 'test/test.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number)
+
+console.log(Math.abs(N - M))
