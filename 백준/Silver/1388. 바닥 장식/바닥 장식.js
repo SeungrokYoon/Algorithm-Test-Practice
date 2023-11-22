4e9d9d24404f05430fb8e211f19ae751f6248f45
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2023-11-22 19:12:15 +0900

    [Silver IV] Title: 바닥 장식, Time: 204 ms, Memory: 12440 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.js
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.js
@@ -7,16 +7,20 @@ const input = require('fs')
 const [N, M] = input[0].split(' ').map(Number)
 const floor = input.slice(1).map((s) => s.split(''))
 
+const VISITED_PANEL = 'o'
+const HORIZONTAL_PANEL = '-'
+const VERTICAL_PANEL = '|'
+
 const visitWithBFS = (i, j) => {
-  const dx = floor[i][j] === '-' ? [0, 0] : [1, -1]
-  const dy = floor[i][j] === '-' ? [1, -1] : [0, 0]
-  const target = floor[i][j] === '-' ? '-' : '|'
+  const dx = floor[i][j] === HORIZONTAL_PANEL ? [0, 0] : [1, -1]
+  const dy = floor[i][j] === HORIZONTAL_PANEL ? [1, -1] : [0, 0]
+  const target = floor[i][j] === HORIZONTAL_PANEL ? HORIZONTAL_PANEL : VERTICAL_PANEL
 
   const queue = [[i, j]]
   while (queue.length) {
     const [x, y] = queue.shift()
-    if (floor[x][y] === 'o') continue
-    floor[x][y] = 'o'
+    if (floor[x][y] === VISITED_PANEL) continue
+    floor[x][y] = VISITED_PANEL
     for (let delta = 0; delta < 2; delta++) {
       const [nx, ny] = [x + dx[delta], y + dy[delta]]
       if (
@@ -24,7 +28,7 @@ const visitWithBFS = (i, j) => {
         ny < 0 ||
         nx >= N ||
         ny >= M ||
-        floor[nx][ny] === 'o' ||
+        floor[nx][ny] === VISITED_PANEL ||
         floor[nx][ny] !== target
       )
         continue
@@ -36,7 +40,7 @@ const visitWithBFS = (i, j) => {
 let count = 0
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < M; j++) {
-    if (floor[i][j] === 'o') continue
+    if (floor[i][j] === VISITED_PANEL) continue
     visitWithBFS(i, j)
     count++
   }
