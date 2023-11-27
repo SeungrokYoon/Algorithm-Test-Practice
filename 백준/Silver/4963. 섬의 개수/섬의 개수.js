77b44ee52480a959363fa7bedd6ecf24acdfbb6b
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2023-11-27 22:00:16 +0900

    [Silver II] Title: 섬의 개수, Time: 204 ms, Memory: 12236 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.js
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.js
@@ -1,45 +1,46 @@
 const input = require('fs')
-  .readFileSync('/dev/stdin')
+  .readFileSync(process.platform === 'linux' ? 'dev/stdin' : 'test/test.txt')
   .toString()
   .trim()
   .split('\n')
 
-const solution = () => {
-  const [w, h] = input.shift().split(' ').map(Number)
-  const graph = []
-  let lands = 0
-  for (let i = 0; i < h; i++) {
-    graph.push(input.shift().split(' ').map(Number))
+input.pop()
+
+const VISITED = -1
+const SEA = 0
+const dRow = [-1, -1, 0, 1, 1, 1, 0, -1]
+const dCol = [0, 1, 1, 1, 0, -1, -1, -1]
+const isValidCoords = (map, row, col, W, H) =>
+  row >= 0 && row < H && col >= 0 && col < W && map[row][col] !== VISITED && map[row][col] !== SEA
+
+const answer = []
+
+const dfs = (map, row, col, H, W) => {
+  map[row][col] = VISITED
+  for (let deltaIndex = 0; deltaIndex < 8; deltaIndex++) {
+    const [nRow, nCol] = [row + dRow[deltaIndex], col + dCol[deltaIndex]]
+    if (!isValidCoords(map, nRow, nCol, W, H)) continue
+    dfs(map, nRow, nCol, H, W)
   }
-  const dfs = (i, j) => {
-    const dRow = [0, 0, 1, -1, 1, 1, -1, -1]
-    const dCol = [1, -1, 0, 0, 1, -1, 1, -1]
-    graph[i][j] = 0
-    for (let index = 0; index < 8; index++) {
-      const nextRow = i + dRow[index]
-      const nextCol = j + dCol[index]
-      if (
-        nextRow < 0 ||
-        nextRow >= h ||
-        nextCol < 0 ||
-        nextCol >= w ||
-        graph[nextRow][nextCol] === 0
-      ) {
-        continue
-      }
-      dfs(nextRow, nextCol)
-    }
-  }
-  for (let i = 0; i < h; i++) {
-    for (let j = 0; j < w; j++) {
-      if (graph[i][j] === 0) continue
-      lands++
-      dfs(i, j)
-    }
-  }
-  return lands
 }
 
-while (input[0] !== '0 0') {
-  console.log(solution())
+const solution = (map, W, H) => {
+  let islands = 0
+  for (let row = 0; row < H; row++) {
+    for (let col = 0; col < W; col++) {
+      if (map[row][col] === VISITED || map[row][col] === SEA) continue
+      dfs(map, row, col, H, W)
+      islands++
+    }
+  }
+  return islands
 }
+
+for (let i = 0; i < input.length; i++) {
+  const [W, H] = input[i].split(' ').map(Number)
+  const map = input.slice(i + 1, i + 1 + H).map((s) => s.split(' ').map(Number))
+  answer.push(solution(map, W, H))
+  i += H
+}
+
+console.log(answer.join('\n'))
