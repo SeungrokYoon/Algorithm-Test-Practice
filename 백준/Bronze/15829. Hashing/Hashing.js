c6dae49048ac47806649619312dba128ae40d7c4
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2023-11-28 11:04:17 +0900

    [Bronze II] Title: Hashing, Time: 160 ms, Memory: 9360 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -6,14 +6,22 @@ const input = require('fs')
 const N = +input[0]
 const str = input[1].toLowerCase()
 
+const rep = (r, i, mod) => {
+  let m = 1
+  for (let j = 0; j < i; j++) {
+    m = (m * r) % mod
+  }
+  return m
+}
+
 const hashFunction = (str) => {
   const r = 31
   const M = 1234567891
-  let s = 0
+  let s = BigInt(0)
   for (let i = 0; i < str.length; i++) {
-    s += (str.charCodeAt(i) - 96) * r ** i
+    s += (BigInt(str.charCodeAt(i) - 96) * BigInt(rep(r, i, M))) % BigInt(M)
   }
-  return s % M
+  return (s % BigInt(M)).toString()
 }
 
 console.log(hashFunction(str))
