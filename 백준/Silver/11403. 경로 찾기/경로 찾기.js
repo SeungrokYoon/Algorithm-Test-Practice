1fd12f84f3025c674a66873d8a8ab842182c8fca
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2023-12-07 02:37:24 +0900

    [Silver I] Title: 경로 찾기, Time: 200 ms, Memory: 10032 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.js
@@ -0,0 +1,25 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? 'dev/stdin' : 'test/test.txt')
+  .toString()
+  .trim()
+  .split('\n')
+const N = +input[0]
+const adjMatrix = input.slice(1).map((l) => l.split(' ').map(Number))
+
+for (let i = 0; i < N; i++) {
+  for (let mid = 0; mid < N; mid++) {
+    for (let j = 0; j < N; j++) {
+      if (adjMatrix[i][mid] && adjMatrix[mid][j]) adjMatrix[i][j] = 1
+    }
+  }
+}
+
+for (let i = 0; i < N; i++) {
+  for (let mid = 0; mid < N; mid++) {
+    for (let j = 0; j < N; j++) {
+      if (adjMatrix[i][mid] && adjMatrix[mid][j]) adjMatrix[i][j] = 1
+    }
+  }
+}
+
+console.log(adjMatrix.map((l) => l.join(' ')).join('\n'))
