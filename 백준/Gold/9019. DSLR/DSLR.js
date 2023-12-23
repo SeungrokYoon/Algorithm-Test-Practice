dc8b547b6895103540165df5d3ef4c8cba84c006
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2023-12-23 22:52:14 +0900

    [Gold IV] Title: DSLR, Time: 10656 ms, Memory: 47876 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.js
+++ 백준/Gold/9019. DSLR/DSLR.js
@@ -1,47 +1,45 @@
 const input = require('fs')
-  .readFileSync(process.platform == 'linux' ? 'dev/stdin' : __dirname + '/../input.txt')
+  .readFileSync(process.platform == 'linux' ? 'dev/stdin' : 'test/test.txt')
   .toString()
   .trim()
   .split('\n')
 
-const F = (n) => [
-  Math.floor(n / 1000),
-  Math.floor((n % 1000) / 100),
-  Math.floor((n % 100) / 10),
-  n % 10,
-]
-const D = (n) => [(2 * n) % 10000, 'D']
-const S = (n) => [n == 0 ? 9999 : n - 1, 'S']
-const L = (n) => {
-  const [a, b, c, d] = F(n)
-  return [b * 1000 + c * 100 + d * 10 + a, 'L']
+function executeD(register) {
+  return register * 2 > 9999 ? (register * 2) % 10000 : register * 2
 }
-const R = (n) => {
-  const [a, b, c, d] = F(n)
-  return [d * 1000 + a * 100 + b * 10 + c, 'R']
+function executeS(register) {
+  return (register + 9999) % 10000
 }
-const DSLR = [(n) => D(n), (n) => S(n), (n) => L(n), (n) => R(n)]
+function executeL(register) {
+  return ((register * 10) % 10000) + Math.floor(register / 1000)
+}
+function executeR(register) {
+  return (register % 10) * 1000 + Math.floor(register / 10)
+}
+
+const DSLR = [executeD, executeS, executeL, executeR]
 
 const answer = []
-input.slice(1).forEach((line) => {
-  const [A, B] = line.split(' ').map(Number)
-  const queue = [[A, '']]
-  const visited = Array(10000).fill(0)
-  visited[A] = 1
 
+input.slice(1).forEach((l) => {
+  const [startNum, endNum] = l.split(' ').map(Number)
+  const queue = [[startNum, '']]
+  const visited = Array(10000).fill(0)
+  visited[startNum] = 1
   let head = 0
   while (queue.length > head) {
-    const [now, history] = queue[head++]
-    if (now === B) {
+    const [num, history] = queue[head++]
+    visited[num] = 1
+    if (num === endNum) {
       answer.push(history)
       break
     }
-
+    const command = ['D', 'S', 'L', 'R']
     for (let i = 0; i < 4; i++) {
-      const [next, cmd] = DSLR[i](now)
-      if (-1 < next && next < 10000 && !visited[next]) {
-        queue.push([next, history + cmd])
-        visited[next] = 1
+      const nextNum = DSLR[i](num)
+      if (0 <= nextNum && nextNum < 10000 && !visited[nextNum]) {
+        queue.push([nextNum, history + command[i]])
+        visited[nextNum] = 1
       }
     }
   }
