c7f348db0c748e07e943a7f886450956983d01bc
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2024-01-03 00:13:24 +0900

    [Silver I] Title: 태권왕, Time: 192 ms, Memory: 11912 KB -BaekjoonHub

--- 백준/Silver/14562. 태권왕/태권왕.js
+++ 백준/Silver/14562. 태권왕/태권왕.js
@@ -13,29 +13,20 @@ input.slice(1).forEach((l) => {
 
 function solution(S, T) {
   const queue = [[S, T, 0]]
-  const counterMap = new Map()
-  counterMap.set(S, 0)
   let kicks = Infinity
   while (queue.length) {
     const [currNum, nextT, count] = queue.shift()
-    // console.log(currNum, nextT, count)
-    if (currNum === nextT) {
-      // console.log('kick')
-      kicks = Math.min(kicks, count)
-      continue
-    }
+    if (currNum === nextT) return count
     const s1 = currNum * 2
     const t1 = nextT + 3
     if (s1 <= t1) {
       const min = count + 1
-      counterMap.set(s1, min)
       queue.push([s1, t1, min])
     }
     const s2 = currNum + 1
     const t2 = nextT
     if (s2 <= t2) {
       const min = count + 1
-      counterMap.set(s2, min)
       queue.push([s2, t2, min])
     }
   }
