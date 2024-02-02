b358a9d346067332464ce8b72149d45d3050fb4a
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2024-02-03 06:14:05 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 760 ms, Memory: 67512 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.js
@@ -0,0 +1,15 @@
+//https://www.acmicpc.net/submit/11478
+
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? 'dev/stdin' : 'test/test.txt')
+  .toString()
+  .trim()
+
+const subStrSet = new Set()
+for (let start = 0; start < input.length; start++) {
+  for (let len = 1; start + len - 1 < input.length; len++) {
+    const subStr = input.slice(start, start + len)
+    if (!subStrSet.has(subStr)) subStrSet.add(subStr)
+  }
+}
+console.log(subStrSet.size)
