922dcae14c234bf182983fdae8b0e1a26d0e95bd
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2024-02-14 21:26:18 +0900

    [Silver II] Title: 예산, Time: 184 ms, Memory: 10428 KB -BaekjoonHub

---
+++ 백준/Silver II/2512. 예산/예산.js
@@ -0,0 +1,37 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? 'dev/stdin' : 'test/test.txt')
+  .toString()
+  .trim()
+  .split('\n')
+
+/**nlogn 풀이법을 고안해보자 */
+
+const BUDGET_REQUESTS = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b)
+const TOTAL_BUDGET = +input[2]
+
+const solution = () => {
+  let left = 0
+  let right = BUDGET_REQUESTS[BUDGET_REQUESTS.length - 1]
+  let optimal = 0
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+    const calculatedTotal = BUDGET_REQUESTS.reduce((acc, curr) => {
+      if (curr > mid) return acc + mid
+      return acc + curr
+    }, 0)
+    if (calculatedTotal <= TOTAL_BUDGET) {
+      left = mid + 1
+      optimal = Math.max(optimal, mid)
+    } else if (calculatedTotal < TOTAL_BUDGET) {
+      left = mid + 1
+    } else {
+      right = mid - 1
+    }
+  }
+  return optimal
+}
+
+console.log(solution())
